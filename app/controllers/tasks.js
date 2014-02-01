c206be9d710f7cf09f35ee0adf9d67f76a462a3c
Michael Jeffrey <michaeldjeffrey@gmail.com>
2014-02-01 09:40:09 -0700

    bring back the add task stuff

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -1,57 +1,43 @@
 var TasksController =  Ember.ArrayController.extend({
     itemController: 'task',
     // showForm: false,
-    // numCounting: function(){
-        // this.filter(function(item, index, enumerable){
-            // console.log(item.get('started'))
-// 
-        // })
-        // return this.filterProperty('started', true).length
-    // }.property('@each.started', '@each.time'),
-// 
-    // onTick: function(task){
-        // var e = this.getElapsed();
-        // var string = e.hours +":"+ e.minutes +":"+ e.seconds;
-        // task.set('time', string);
-    // },
-// 
-    // actions: {
-        // add: function(){
-            // this.set('showForm', true);
-        // },
-        // cancel: function(){
-            // this.set('showForm', false);
-            // this.set('newTitle', '');
-            // this.set('newAuthor', '');
-            // this.set('newContents', '');
-        // },
-        // createTask: function(){
-            // var title, author, contents, task;
-// 
-            // title = this.get('newTitle').trim();
-            // author = this.get('newAuthor').trim();
-            // contents = this.get('newContents').trim();
-// 
-            // if(!title || !author || !contents){
-                // return;
-            // }
-// 
-            // console.log(title, author, contents)
-// 
-            // task = this.store.createRecord('task', {
-                // title: title,
-                // author: author,
-                // contents: contents
-            // });
-            // task.save();
-// 
-            // this.set('newTitle', '');
-            // this.set('newAuthor', '');
-            // this.set('newContents', '');
-// 
-            // this.set('showForm', false);
-        // }
-    // },
+    actions: {
+        add: function(){
+            this.set('showForm', true);
+        },
+        cancel: function(){
+            this.set('showForm', false);
+            this.set('newTitle', '');
+            this.set('newAuthor', '');
+            this.set('newContents', '');
+        },
+        createTask: function(){
+            var title, author, contents, task;
+
+            title = this.get('newTitle').trim();
+            author = this.get('newAuthor').trim();
+            contents = this.get('newContents').trim();
+
+            if(!title || !author || !contents){
+                return;
+            }
+
+            console.log(title, author, contents)
+
+            task = this.store.createRecord('task', {
+                title: title,
+                author: author,
+                contents: contents
+            });
+            task.save();
+
+            this.set('newTitle', '');
+            this.set('newAuthor', '');
+            this.set('newContents', '');
+
+            this.set('showForm', false);
+        }
+    },
 })
 
 export default TasksController;
