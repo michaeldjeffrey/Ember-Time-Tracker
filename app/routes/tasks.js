4c83438cf1be2abe457012536482ebb4d2ba9639
Michael Jeffrey <michaeldjeffrey@gmail.com>
2014-02-10 18:55:21 -0700

    looking at a tutorial, don't need to set up the controller, and the model is much easier

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -1,11 +1,6 @@
 var TasksRoute = Ember.Route.extend({
     model: function(){
-        var store = this.get('store');
-        store.find('task');
-        return store.all('task');
-    },
-    setupController: function(controller, model){
-        controller.set('model', model);
+        return this.store.find('task');
     },
 })
 
