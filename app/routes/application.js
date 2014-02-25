e408a84f3d10d6801a13d84646495a8ee7581c30
Michael Jeffrey <michaeldjeffrey@gmail.com>
2014-02-24 22:52:57 -0700

    add a user to be used at the base level

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,14 +2,15 @@ var ApplicationRoute = Ember.Route.extend({
     model: function(){
         this.store.push('User', {
             id: 1,
-            username: 'migpok35',
-            first_name: '',
-            last_name: '',
-            email: '',
-            phone_number: '',
+            username: 'hunter1',
+            first_name: 'Michael',
+            last_name: 'Jeffrey',
+            email: 'hunter1@mailinator.com',
+            phone_number: '480-555-5555',
             save_frequency: 5000
         });
-    }
+        return this.store.find('user', 1);
+    },
 });
 
 export default ApplicationRoute;
