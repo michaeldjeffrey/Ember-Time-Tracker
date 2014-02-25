db02be2cece85465e9993ac4f9964584200ad540
Michael Jeffrey <michaeldjeffrey@gmail.com>
2014-02-24 21:49:11 -0700

    add a user model when the application is started

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,3 +1,15 @@
-export default Ember.Route.extend({
-    save_interval: 5000
+var ApplicationRoute = Ember.Route.extend({
+    model: function(){
+        this.store.push('User', {
+            id: 1,
+            username: 'migpok35',
+            first_name: '',
+            last_name: '',
+            email: '',
+            phone_number: '',
+            save_frequency: 5000
+        });
+    }
 });
+
+export default ApplicationRoute;
